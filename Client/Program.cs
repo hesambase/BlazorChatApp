6083e69b2ec6a253fb2bd018043d1f34434cb918
hesam attari <Hesam_att@outlook.com>
2022-09-06 13:12:44 +0430

    Client App is Completed.

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,5 @@
 using BlazorChatApp.Client;
-using BlazorChatApp.Client.Manager;
+using BlazorChatApp.Client.Managers;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
